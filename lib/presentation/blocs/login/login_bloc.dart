060b83295bb910f7f10de5f2ead3fba1871ffa24
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-07-31 01:29:03 +0530

    Update login_bloc.dart

--- lib/presentation/blocs/login/login_bloc.dart
+++ lib/presentation/blocs/login/login_bloc.dart
@@ -1,15 +1,30 @@
 import 'dart:async';
-
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
-
+import 'package:notex/core/repositories/auth_repository.dart';
 part 'login_event.dart';
 part 'login_state.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
-  LoginBloc() : super(LoginInitial()) {
-    on<LoginEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  LoginBloc() : super(LoginInitialState()) {
+    on<LoginPageLoginButtonClickedEvent>(processLogin);
+  }
+  FutureOr<void> processLogin(LoginPageLoginButtonClickedEvent event, Emitter<LoginState> emit) async{
+    // login user
+    try{
+      emit(LoginLoadingSate());
+      await AuthRepository.loginUser(event.email, event.password, event.rememberDevice).then(
+          (response) {
+            if(response.success){
+              emit(LoginSuccessState());
+            } else{
+              emit(LoginFailedState(response.message));
+            }
+          }
+      );
+      emit(LoginLoadedState());
+    } catch(error){
+      emit(LoginFailedState("An unexpected error occurred"));
+    }
   }
 }
