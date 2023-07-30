f6a252be83b3a34ca2d5cbbe304eb643119d5c3d
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-07-31 01:28:28 +0530

    Create token_data_entity.dart

---
+++ lib/core/entities/token_data_entity.dart
@@ -0,0 +1,11 @@
+class TokenDataEntity {
+  final String userId;
+  final String name;
+  final String email;
+
+  TokenDataEntity({
+    required this.userId,
+    required this.name,
+    required this.email,
+  });
+}
