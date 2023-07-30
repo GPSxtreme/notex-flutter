b26c748bb64135f8035fda079a0a2750d445e78a
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-07-31 01:28:17 +0530

    Create api_routes.dart

---
+++ lib/core/config/api_routes.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: constant_identifier_names, non_constant_identifier_names
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+final _apiEndPoint = dotenv.env["API_ENDPOINT"];
+/* start of user routes */
+final USER_LOGIN_ROUTE = '$_apiEndPoint/user/login';
+final USER_REGISTER_ROUTE = '$_apiEndPoint/user/register';
+final USER_PROFILE_PICTURE_UPLOAD_ROUTE = '$_apiEndPoint/user/uploadProfilePicture';
+final USER_PROFILE_PICTURE_GET_ROUTE = '$_apiEndPoint/user/getProfilePicture';
+/* end of user routes */
+
+/* start of todo routes */
+final TODO_ADD_ROUTE = '$_apiEndPoint/todo/addTodo';
+final TODO_GET_ROUTE = '$_apiEndPoint/todo/getTodos';
+/// Required todoId in params
+final TODO_DELETE_ROUTE = '$_apiEndPoint/todo/deleteTodo';
+/* end of todo routes */
+
+/*start of note routes */
+final NOTE_ADD_ROUTE = '$_apiEndPoint/note/addNote';
+final NOTE_GET_ROUTE = '$_apiEndPoint/note/getNotes';
+/// Required noteId in params
+final NOTE_DELETE_ROUTE = '$_apiEndPoint/note/deleteNote';
