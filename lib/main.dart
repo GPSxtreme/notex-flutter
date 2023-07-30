2fde39892cc94f4e7dad7ce847bacfb34dacd3ed
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-07-31 01:28:57 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'app.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async{
+  await dotenv.load();
   runApp(const MyApp());
 }
 
