e8d0097b78c8161311a5610058ff19d44b429d53
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-07-31 01:28:36 +0530

    Create jwt_decoder_repository.dart

---
+++ lib/core/repositories/jwt_decoder_repository.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/foundation.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
+
+class JwtDecoderRepository {
+  // Method to decode the JWT token and extract user data
+  Map<String, dynamic>? decodeJwtToken(String token) {
+    try {
+      Map<String, dynamic> decodedToken = JwtDecoder.decode(token);
+      return decodedToken;
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error decoding JWT token: $e');
+      }
+      return null;
+    }
+  }
+  // Method to verify the validity of the JWT token
+  static bool verifyJwtToken(String token) {
+    try {
+      DateTime expirationDate = JwtDecoder.getExpirationDate(token);
+      bool isExpired = DateTime.now().isAfter(expirationDate);
+      return !isExpired;
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error verifying JWT token: $e');
+      }
+    }
+    return false;
+  }
+
+}
