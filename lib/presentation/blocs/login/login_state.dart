2deef5d854683f71e795064d63880dc15a8ffe7b
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-07-31 01:29:06 +0530

    Update login_state.dart

--- lib/presentation/blocs/login/login_state.dart
+++ lib/presentation/blocs/login/login_state.dart
@@ -2,5 +2,19 @@ part of 'login_bloc.dart';
 
 @immutable
 abstract class LoginState {}
+abstract class LoginActionState extends LoginState {}
 
-class LoginInitial extends LoginState {}
+class LoginInitialState extends LoginState {}
+
+class LoginLoadingSate extends LoginState {}
+
+class LoginLoadedState extends LoginState {}
+
+class LoginFailedState extends LoginActionState {
+  final String reason;
+  LoginFailedState(this.reason);
+}
+
+class LoginSuccessState extends LoginActionState {}
+
+class LoginNavigateToRegisterPageActionState extends LoginActionState {}
