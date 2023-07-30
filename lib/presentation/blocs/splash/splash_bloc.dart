9b476dda795804b8f8c7e518d2e6ed7238ddffd4
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-07-31 01:29:10 +0530

    Update splash_bloc.dart

--- lib/presentation/blocs/splash/splash_bloc.dart
+++ lib/presentation/blocs/splash/splash_bloc.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:notex/core/repositories/jwt_decoder_repository.dart';
+import 'package:notex/core/repositories/shared_preferences_repository.dart';
 part 'splash_event.dart';
 part 'splash_state.dart';
 
@@ -12,12 +14,15 @@ class SplashBloc extends Bloc<SplashEvent, SplashState> {
   FutureOr<void> splashInitialEvent(
       SplashInitialEvent event, Emitter<SplashState> emit) async {
     emit(SplashLoadingState());
-    await Future.delayed(const Duration(seconds: 4));
-    // perform splash operations
-    // --------------------------------
-    // if user is authenticated
-    // emit(SplashUserAuthenticatedState());
-    // if not authenticated
-    emit(SplashUserNotAuthenticatedState());
+    await Future.delayed(const Duration(seconds: 3));
+    String? userToken = await SharedPreferencesRepository.getJwtToken();
+    if(userToken != null){
+      bool isValid =  JwtDecoderRepository.verifyJwtToken(userToken);
+      if(isValid){
+        emit(SplashUserAuthenticatedState());
+      }
+    } else {
+      emit(SplashUserNotAuthenticatedState());
+    }
   }
 }
