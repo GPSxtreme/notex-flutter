74328ca1ccfc4fba4d22bb334df3822317a1aef7
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-07-31 01:29:05 +0530

    Update login_event.dart

--- lib/presentation/blocs/login/login_event.dart
+++ lib/presentation/blocs/login/login_event.dart
@@ -2,3 +2,13 @@ part of 'login_bloc.dart';
 
 @immutable
 abstract class LoginEvent {}
+
+
+class LoginPageLoginButtonClickedEvent extends LoginEvent {
+  final String email;
+  final String password;
+  final bool rememberDevice;
+  LoginPageLoginButtonClickedEvent(this.email, this.password, this.rememberDevice);
+}
+
+class LoginPageRegisterButtonClickedEvent extends LoginEvent {}
