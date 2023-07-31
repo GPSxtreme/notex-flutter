73a17dc5287e3e053eb090673295a09b396f46fa
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-07-31 19:57:41 +0530

    Update register_bloc.dart

--- lib/presentation/blocs/register/register_bloc.dart
+++ lib/presentation/blocs/register/register_bloc.dart
@@ -2,14 +2,43 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:notex/core/repositories/auth_repository.dart';
 
 part 'register_event.dart';
 part 'register_state.dart';
 
 class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
-  RegisterBloc() : super(RegisterInitial()) {
-    on<RegisterEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  RegisterBloc() : super(RegisterInitialState()) {
+    on<RegisterPageRegisterButtonPressedEvent>(processRegisterUser);
+    on<RegisterPageLoginButtonPressedEvent>(processRedirectToLoginPage);
+    on<RegisterPageEmptyCredentialsEvent>(processEmptyCredentialsError);
+    on<RegisterPagePasswordsDoNotMatchEvent>(processPasswordsDoNotMatchError);
+  }
+
+  FutureOr<void> processRegisterUser(RegisterPageRegisterButtonPressedEvent event, Emitter<RegisterState> emit) async{
+    try{
+      emit(RegisterLoadingState());
+      await AuthRepository.registerUser(event.email, event.password, event.rememberDevice).then(
+              (response) {
+            if(response.success){
+              emit(RegisterSuccessState());
+            } else{
+              emit(RegisterFailedState(response.message));
+            }
+          }
+      );
+      emit(RegisterLoadedState());
+    } catch(error){
+      emit(RegisterFailedState("An unexpected error occurred"));
+    }
+  }
+  FutureOr<void> processRedirectToLoginPage(RegisterPageLoginButtonPressedEvent event, Emitter<RegisterState> emit) {
+  emit(RegisterRedirectToLoginState());
+  }
+  FutureOr<void> processEmptyCredentialsError(RegisterPageEmptyCredentialsEvent event, Emitter<RegisterState> emit) {
+    emit(RegisterEmptyCredentialsState());
+  }
+  FutureOr<void> processPasswordsDoNotMatchError(RegisterPagePasswordsDoNotMatchEvent event, Emitter<RegisterState> emit) {
+    emit(RegisterPasswordsDoNotMatchState());
   }
 }
