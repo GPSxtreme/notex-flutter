21c33dbae0fc894d3346d8fe78a0e65fba333ac0
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-07-31 19:57:43 +0530

    Update register_event.dart

--- lib/presentation/blocs/register/register_event.dart
+++ lib/presentation/blocs/register/register_event.dart
@@ -2,3 +2,16 @@ part of 'register_bloc.dart';
 
 @immutable
 abstract class RegisterEvent {}
+
+class RegisterPageEmptyCredentialsEvent extends RegisterEvent {}
+
+class RegisterPagePasswordsDoNotMatchEvent extends RegisterEvent {}
+
+class RegisterPageRegisterButtonPressedEvent extends RegisterEvent {
+  final String email;
+  final String password;
+  final bool rememberDevice;
+  RegisterPageRegisterButtonPressedEvent(this.email, this.password, this.rememberDevice);
+}
+
+class RegisterPageLoginButtonPressedEvent extends RegisterEvent {}
