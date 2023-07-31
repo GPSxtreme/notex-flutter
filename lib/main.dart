ed25d56f18c4fd4b11e18d06b609ed75add4a382
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-07-31 19:57:34 +0530

    updated status bar color

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:notex/presentation/styles/app_styles.dart';
 import 'app.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async{
   await dotenv.load();
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: kPageBgStart,
+  ));
   runApp(const MyApp());
 }
 
