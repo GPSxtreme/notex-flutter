30b27dda6a6385dd32515b633488bd58ec358ebc
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-07-31 19:56:35 +0530

    Create create_user_profile.dart

---
+++ lib/presentation/pages/create_user_profile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreateUserProfile extends StatefulWidget {
+  const CreateUserProfile({super.key});
+
+  @override
+  State<CreateUserProfile> createState() => _CreateUserProfileState();
+}
+
+class _CreateUserProfileState extends State<CreateUserProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
