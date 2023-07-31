f683bd4f5a566fc6beffb00ab85aaceb079869cc
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-07-31 19:57:04 +0530

    Changed user_register_route wrong url

--- lib/core/config/api_routes.dart
+++ lib/core/config/api_routes.dart
@@ -5,17 +5,17 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 final _apiEndPoint = dotenv.env["API_ENDPOINT"];
 /* start of user routes */
 final USER_LOGIN_ROUTE = '$_apiEndPoint/user/login';
-final USER_REGISTER_ROUTE = '$_apiEndPoint/user/register';
+final USER_REGISTER_ROUTE = '$_apiEndPoint/user/registration';
 final USER_PROFILE_PICTURE_UPLOAD_ROUTE = '$_apiEndPoint/user/uploadProfilePicture';
 final USER_PROFILE_PICTURE_GET_ROUTE = '$_apiEndPoint/user/getProfilePicture';
 /* end of user routes */
 
-/* start of todo routes */
+/* start of todo_routes */
 final TODO_ADD_ROUTE = '$_apiEndPoint/todo/addTodo';
 final TODO_GET_ROUTE = '$_apiEndPoint/todo/getTodos';
 /// Required todoId in params
 final TODO_DELETE_ROUTE = '$_apiEndPoint/todo/deleteTodo';
-/* end of todo routes */
+/* end of todo_routes */
 
 /*start of note routes */
 final NOTE_ADD_ROUTE = '$_apiEndPoint/note/addNote';
