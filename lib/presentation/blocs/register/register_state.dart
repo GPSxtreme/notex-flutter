ac8d30221eb071f6763fb8a210a466aafe3d65ca
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-07-31 19:57:44 +0530

    Update register_state.dart

--- lib/presentation/blocs/register/register_state.dart
+++ lib/presentation/blocs/register/register_state.dart
@@ -3,4 +3,23 @@ part of 'register_bloc.dart';
 @immutable
 abstract class RegisterState {}
 
-class RegisterInitial extends RegisterState {}
+abstract class RegisterActionState extends RegisterState {}
+
+class RegisterInitialState extends RegisterState {}
+
+class RegisterLoadingState extends RegisterState{}
+
+class RegisterLoadedState extends RegisterState {}
+
+class RegisterEmptyCredentialsState extends RegisterActionState {}
+
+class RegisterPasswordsDoNotMatchState extends RegisterActionState {}
+
+class RegisterFailedState extends RegisterActionState {
+  final String reason;
+  RegisterFailedState(this.reason);
+}
+
+class RegisterSuccessState extends RegisterActionState {}
+
+class RegisterRedirectToLoginState extends RegisterActionState {}
