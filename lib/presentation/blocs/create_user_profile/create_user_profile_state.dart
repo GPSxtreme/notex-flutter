c1037985a0653522b756e72f10626e3e946e4cf5
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-08-01 22:05:14 +0530

    Update create_user_profile_state.dart

--- lib/presentation/blocs/create_user_profile/create_user_profile_state.dart
+++ lib/presentation/blocs/create_user_profile/create_user_profile_state.dart
@@ -3,4 +3,19 @@ part of 'create_user_profile_bloc.dart';
 @immutable
 abstract class CreateUserProfileState {}
 
+abstract class CreateUserProfileActionState extends CreateUserProfileState{}
+
 class CreateUserProfileInitial extends CreateUserProfileState {}
+
+class CreateUserProfileLoadingState extends CreateUserProfileState {}
+
+class CreateUserProfileLoadedState extends CreateUserProfileState{}
+
+class CreateUserProfileSuccessState extends CreateUserProfileActionState {}
+
+class CreateUserProfileFailedState extends CreateUserProfileActionState {
+  final String reason;
+  CreateUserProfileFailedState(this.reason);
+}
+
+class CreateUserProfileOpenDatePickerState extends CreateUserProfileActionState{}
