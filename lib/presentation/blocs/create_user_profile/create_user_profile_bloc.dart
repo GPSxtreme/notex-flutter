24338119babb391cac54447aa032084e1f9798c4
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-08-01 22:05:08 +0530

    Update create_user_profile_bloc.dart

--- lib/presentation/blocs/create_user_profile/create_user_profile_bloc.dart
+++ lib/presentation/blocs/create_user_profile/create_user_profile_bloc.dart
@@ -1,15 +1,42 @@
 import 'dart:async';
-
+import 'dart:io';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:notex/core/repositories/user_repository.dart';
+
+import '../../../data/models/updatable_user_data_model.dart';
 
 part 'create_user_profile_event.dart';
 part 'create_user_profile_state.dart';
 
 class CreateUserProfileBloc extends Bloc<CreateUserProfileEvent, CreateUserProfileState> {
   CreateUserProfileBloc() : super(CreateUserProfileInitial()) {
-    on<CreateUserProfileEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+    on<CreateUserProfileOpenDatePickerEvent>((event, emit) => emit(CreateUserProfileOpenDatePickerState()));
+    on<CreateUserProfileCreateEvent>(handleUserProfileCreate);
+  }
+
+  FutureOr<void> handleUserProfileCreate(CreateUserProfileCreateEvent event,emit)async{
+    // upload profile picture
+    try{
+      emit(CreateUserProfileLoadingState());
+      await UserRepository.updateUserProfilePicture(event.imageFile).then(
+          (_)async{
+            // update remaining data
+            await UserRepository.updateUserData(event.data).then(
+                (response) {
+                  if(response.success){
+                    emit(CreateUserProfileSuccessState());
+                  }else{
+                    emit(CreateUserProfileFailedState(response.message));
+                  }
+                }
+            );
+          }
+      );
+    }catch(e){
+      emit(CreateUserProfileFailedState('Unexpected error occurred : $e'));
+    } finally{
+      emit(CreateUserProfileLoadedState());
+    }
   }
 }
