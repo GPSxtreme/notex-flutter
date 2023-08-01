896fdfcb6fdb0878609c0c30f3ae08bb2d91642b
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-08-01 22:05:12 +0530

    Update create_user_profile_event.dart

--- lib/presentation/blocs/create_user_profile/create_user_profile_event.dart
+++ lib/presentation/blocs/create_user_profile/create_user_profile_event.dart
@@ -2,3 +2,11 @@ part of 'create_user_profile_bloc.dart';
 
 @immutable
 abstract class CreateUserProfileEvent {}
+
+class CreateUserProfileOpenDatePickerEvent extends CreateUserProfileEvent{}
+
+class CreateUserProfileCreateEvent extends CreateUserProfileEvent {
+  final UpdatableUserDataModel data;
+  final File imageFile;
+  CreateUserProfileCreateEvent(this.data, this.imageFile);
+}
