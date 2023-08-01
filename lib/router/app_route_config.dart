21b74f034ff0d9f59a84ca6d97095d48c63067e3
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-08-02 02:39:03 +0530

    Update app_route_config.dart

--- lib/router/app_route_config.dart
+++ lib/router/app_route_config.dart
@@ -4,6 +4,7 @@ import 'package:notex/presentation/pages/create_user_profile.dart';
 import 'package:notex/presentation/pages/register.dart';
 import 'package:notex/presentation/pages/splash.dart';
 import 'package:notex/router/app_route_constants.dart';
+import 'package:notex/router/router_transition_factory.dart';
 import '../presentation/pages/home.dart';
 import '../presentation/pages/login.dart';
 import '../presentation/pages/notes.dart';
@@ -11,9 +12,17 @@ import '../presentation/pages/todos.dart';
 
 class MyAppRouter {
   static GoRouter getRouter(bool isAuth) {
+    // private navigators
+    final rootNavigatorKey = GlobalKey<NavigatorState>();
+    final shellNavigatorNotesKey =
+        GlobalKey<NavigatorState>(debugLabel: 'NotesShell');
+    final shellNavigatorTodosKey =
+        GlobalKey<NavigatorState>(debugLabel: 'TodosShell');
+
     return GoRouter(
-        initialLocation: '/createUserProfile',
-        routes: <GoRoute>[
+        initialLocation: '/notes',
+        navigatorKey: rootNavigatorKey,
+        routes: [
           GoRoute(
               name: AppRouteConstants.splashRouteName,
               path: '/',
@@ -38,24 +47,48 @@ class MyAppRouter {
               pageBuilder: (BuildContext context, GoRouterState state) {
                 return const MaterialPage(child: CreateUserProfile());
               }),
-          GoRoute(
-              name: AppRouteConstants.homeRouteName,
-              path: '/home',
-              pageBuilder: (BuildContext context, GoRouterState state) {
-                return const MaterialPage(child: HomePage());
-              }),
-          GoRoute(
-              name: AppRouteConstants.notesRouteName,
-              path: '/home/notes',
-              pageBuilder: (BuildContext context, GoRouterState state) {
-                return const MaterialPage(child: NotesPage());
-              }),
-          GoRoute(
-              name: AppRouteConstants.todosRouteName,
-              path: '/home/todos',
-              pageBuilder: (BuildContext context, GoRouterState state) {
-                return const MaterialPage(child: TodosPage());
-              }),
+          StatefulShellRoute.indexedStack(
+            builder: (context, state, navigationShell) {
+              // the UI shell
+              return HomePage(navigationShell: navigationShell);
+            },
+            branches: [
+              // first branch (notes)
+              StatefulShellBranch(
+                navigatorKey: shellNavigatorNotesKey,
+                routes: [
+                  // top route inside branch
+                  GoRoute(
+                    name: AppRouteConstants.notesRouteName,
+                    path: '/notes',
+                    pageBuilder: (context, state) =>
+                        RouterTransitionFactory.getTransitionPage(
+                            context: context,
+                            state: state,
+                            child: const NotesPage(),
+                            type: 'slide'),
+                  ),
+                ],
+              ),
+              // second branch (todos)
+              StatefulShellBranch(
+                navigatorKey: shellNavigatorTodosKey,
+                routes: [
+                  // top route inside branch
+                  GoRoute(
+                    name: AppRouteConstants.todosRouteName,
+                    path: '/todos',
+                    pageBuilder: (context, state) =>
+                        RouterTransitionFactory.getTransitionPage(
+                            context: context,
+                            state: state,
+                            child: const TodosPage(),
+                            type: 'slide'),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ],
         redirect: (context, state) {
           // if(!isAuth){
