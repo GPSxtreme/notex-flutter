cba7781e71b7a809c604cab88d2a7d8764df7c47
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-08-02 02:39:05 +0530

    Update app_route_constants.dart

--- lib/router/app_route_constants.dart
+++ lib/router/app_route_constants.dart
@@ -3,7 +3,6 @@ class AppRouteConstants {
   static const String loginRouteName = 'login';
   static const String registerRouteName = 'register';
   static const String createUserProfileName = 'createUserProfile';
-  static const String homeRouteName = 'home';
   static const String notesRouteName = 'notes';
   static const String todosRouteName = 'todos';
 }
