45761c87bc50e7925b8151b3f48954bc6f08652b
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-08-02 02:38:49 +0530

    Update create_user_profile.dart

--- lib/presentation/pages/create_user_profile.dart
+++ lib/presentation/pages/create_user_profile.dart
@@ -118,7 +118,7 @@ class _CreateUserProfileState extends State<CreateUserProfile> {
         } else if(state is CreateUserProfileFailedState){
           kSnackBar(context, state.reason);
         } else if(state is CreateUserProfileSuccessState){
-          GoRouter.of(context).pushReplacementNamed(AppRouteConstants.homeRouteName);
+          GoRouter.of(context).pushReplacementNamed(AppRouteConstants.notesRouteName);
         }
       },
       builder: (context, state) {
