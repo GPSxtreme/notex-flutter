01c22ebb5d1af6dc91f48de6870ffdf0b57674de
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-08-02 02:38:58 +0530

    Update notes.dart

--- lib/presentation/pages/notes.dart
+++ lib/presentation/pages/notes.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../styles/app_styles.dart';
+import '../styles/size_config.dart';
 
 class NotesPage extends StatefulWidget {
   const NotesPage({super.key});
@@ -10,6 +14,100 @@ class NotesPage extends StatefulWidget {
 class _NotesPageState extends State<NotesPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    SizeConfig().init(context);
+    return Scaffold(
+      appBar: null,
+      body: Container(
+        width: SizeConfig.screenWidth,
+        height: SizeConfig.screenHeight,
+        decoration: const BoxDecoration(gradient: kPageBgGradient),
+        child: Stack(
+          children: [
+            Positioned(
+              top: 0,
+              bottom: 0,
+              left: SizeConfig.screenWidth! * 0.1,
+              right: SizeConfig.screenWidth! * 0.1,
+              child: SvgPicture.asset(
+                "assets/svg/magnify-glass.svg",
+              ),
+            ),
+            // showed when no notes are found
+            Positioned(
+                top: 0,
+                bottom: 0,
+                left: SizeConfig.screenWidth! * 0.1,
+                right: SizeConfig.screenWidth! * 0.1,
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'No',
+                      style: kInter.copyWith(
+                          fontSize: 30, fontWeight: FontWeight.w500),
+                    ),
+                    SizedBox(
+                      height: SizeConfig.blockSizeHorizontal! * 2,
+                    ),
+                    Text(
+                      ' notes',
+                      style: kInter.copyWith(
+                        color: kPink,
+                        fontSize: 30,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  'Found',
+                  style: kInter.copyWith(
+                      fontSize: 30, fontWeight: FontWeight.w500),
+                ),
+                SizedBox(
+                  height: SizeConfig.blockSizeVertical! * 3,
+                ),
+                Text(
+                  "You can add new note by pressing\nAdd button at the bottom",
+                  style: kInter.copyWith(fontSize: 15, color: kWhite24),
+                  textAlign: TextAlign.center,
+                )
+              ],
+            )),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 0),
+              child: NotificationListener<OverscrollIndicatorNotification>(
+                onNotification: (overScroll) {
+                  overScroll.disallowIndicator();
+                  return true;
+                },
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      // note widgets go here if present
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: kPink,
+        onPressed: () {
+          // create new note
+          print("add note pressed");
+        },
+        child: Icon(
+          Icons.add,
+          color: kWhite,
+          size: SizeConfig.blockSizeVertical! * 5,
+        ),
+      ),
+    );
   }
 }
