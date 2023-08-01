4bd9ddb15c282f82993a6cf8efb976484adfb4da
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-08-01 22:04:49 +0530

    Update api_routes.dart

--- lib/core/config/api_routes.dart
+++ lib/core/config/api_routes.dart
@@ -8,6 +8,8 @@ final USER_LOGIN_ROUTE = '$_apiEndPoint/user/login';
 final USER_REGISTER_ROUTE = '$_apiEndPoint/user/registration';
 final USER_PROFILE_PICTURE_UPLOAD_ROUTE = '$_apiEndPoint/user/uploadProfilePicture';
 final USER_PROFILE_PICTURE_GET_ROUTE = '$_apiEndPoint/user/getProfilePicture';
+final USER_UPDATE_USER_DATA = '$_apiEndPoint/user/updateUserData';
+final USER_UPLOAD_PROFILE_PIC = '$_apiEndPoint/user/uploadProfilePicture';
 /* end of user routes */
 
 /* start of todo_routes */
@@ -22,3 +24,4 @@ final NOTE_ADD_ROUTE = '$_apiEndPoint/note/addNote';
 final NOTE_GET_ROUTE = '$_apiEndPoint/note/getNotes';
 /// Required noteId in params
 final NOTE_DELETE_ROUTE = '$_apiEndPoint/note/deleteNote';
+/*end of note routes */
