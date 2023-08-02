f244519ce4b5dc7407e1640d6d88eb7715df79ae
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-08-02 21:40:35 +0530

    Create todo_model.dart

---
+++ lib/data/models/todo_model.dart
@@ -0,0 +1,53 @@
+// To parse this JSON data, do
+//
+//     final todoModel = todoModelFromJson(jsonString);
+
+import 'dart:convert';
+
+TodoModel todoModelFromJson(String str) => TodoModel.fromJson(json.decode(str));
+
+String todoModelToJson(TodoModel data) => json.encode(data.toJson());
+
+class TodoModel {
+  final String id;
+  final String userId;
+  final String body;
+  final bool isCompleted;
+  final DateTime createdTime;
+  final DateTime editedTime;
+  final DateTime expireTime;
+  final int v;
+
+  TodoModel({
+    required this.id,
+    required this.userId,
+    required this.body,
+    required this.isCompleted,
+    required this.createdTime,
+    required this.editedTime,
+    required this.expireTime,
+    required this.v,
+  });
+
+  factory TodoModel.fromJson(Map<String, dynamic> json) => TodoModel(
+    id: json["_id"],
+    userId: json["userId"],
+    body: json["body"],
+    isCompleted: json["isCompleted"],
+    createdTime: DateTime.parse(json["createdTime"]),
+    editedTime: DateTime.parse(json["editedTime"]),
+    expireTime: DateTime.parse(json["expireTime"]),
+    v: json["__v"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "_id": id,
+    "userId": userId,
+    "body": body,
+    "isCompleted": isCompleted,
+    "createdTime": createdTime.toIso8601String(),
+    "editedTime": editedTime.toIso8601String(),
+    "expireTime": expireTime.toIso8601String(),
+    "__v": v,
+  };
+}
