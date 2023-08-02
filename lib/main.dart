421d3e45fe2ba40ec80f8bda07a23270c50b2248
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-08-02 21:43:09 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:notex/core/repositories/local_database_repository.dart';
 import 'package:notex/presentation/styles/app_styles.dart';
 import 'app.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+// ignore: non_constant_identifier_names
+LocalDatabaseRepository LOCAL_DB = LocalDatabaseRepository();
 
 void main() async{
   await dotenv.load();
