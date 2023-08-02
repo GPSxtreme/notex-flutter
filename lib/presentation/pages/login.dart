5ce418b0d1ebb9e7807f21f6b273dac04ce61ced
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-08-02 21:43:18 +0530

    Update login.dart

--- lib/presentation/pages/login.dart
+++ lib/presentation/pages/login.dart
@@ -51,7 +51,7 @@ class _LoginPageState extends State<LoginPage> {
         if(state is LoginNavigateToRegisterPageActionState){
           GoRouter.of(context).pushNamed(AppRouteConstants.registerRouteName);
         } else if(state is LoginSuccessState){
-          GoRouter.of(context).go("/${AppRouteConstants.notesRouteName}");
+          GoRouter.of(context).pushNamed(AppRouteConstants.homeRouteName);
         } else if(state is LoginFailedState){
           kSnackBar(context, state.reason);
         }
