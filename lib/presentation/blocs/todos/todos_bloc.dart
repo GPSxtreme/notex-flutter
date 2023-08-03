619732aa82fc661a3b3a82c97b58743c559defa4
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-08-03 18:33:51 +0530

    Update todos_bloc.dart

--- lib/presentation/blocs/todos/todos_bloc.dart
+++ lib/presentation/blocs/todos/todos_bloc.dart
@@ -1,15 +1,60 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:connectivity/connectivity.dart';
 import 'package:meta/meta.dart';
+import 'package:notex/core/repositories/todos_repository.dart';
+import 'package:notex/data/models/todo_model.dart';
+import '../../../main.dart';
 
 part 'todos_event.dart';
+
 part 'todos_state.dart';
 
 class TodosBloc extends Bloc<TodosEvent, TodosState> {
-  TodosBloc() : super(TodosInitial()) {
-    on<TodosEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  TodosBloc() : super(TodosInitialState()) {
+    on<TodosInitialEvent>(handleFetchTodos);
+  }
+
+  Future<void> handleFetchTodos(
+      TodosInitialEvent event, Emitter<TodosState> emit) async {
+    try {
+      emit(TodosFetchingState());
+
+      final connectivityResult = await Connectivity().checkConnectivity();
+      final hasInternet = (connectivityResult == ConnectivityResult.mobile ||
+          connectivityResult == ConnectivityResult.wifi);
+
+      bool isFetchedNotesEmpty = false;
+
+      if (hasInternet) {
+        final fetchResponse = await TodosRepository.fetchTodos();
+        if (fetchResponse.success && fetchResponse.todos!.isNotEmpty) {
+          final onlineFetchedNotes = fetchResponse.todos!;
+          final updatedNotes =
+              await TodosRepository.syncOnlineTodos(onlineFetchedNotes);
+
+          if (updatedNotes.isNotEmpty) {
+            emit(TodosFetchedState(updatedNotes));
+          } else {
+            isFetchedNotesEmpty = true;
+          }
+        } else {
+          isFetchedNotesEmpty = true;
+        }
+      }
+
+      if (!hasInternet || isFetchedNotesEmpty) {
+        final offlineFetchedNotes = await LOCAL_DB.getTodos();
+
+        if (offlineFetchedNotes.isEmpty) {
+          emit(TodosEmptyState());
+        } else {
+          emit(TodosFetchedState(offlineFetchedNotes));
+        }
+      }
+    } catch (error) {
+      emit(TodosFetchingFailedState(error.toString()));
+    }
   }
 }
