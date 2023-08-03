669f84d859b6de359b30885de6a937ee4d98a8a6
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-08-03 18:33:49 +0530

    Update notes_state.dart

--- lib/presentation/blocs/notes/notes_state.dart
+++ lib/presentation/blocs/notes/notes_state.dart
@@ -3,4 +3,27 @@ part of 'notes_bloc.dart';
 @immutable
 abstract class NotesState {}
 
-class NotesInitial extends NotesState {}
+abstract class NotesActionState extends NotesState {}
+
+class NotesInitialState extends NotesState {}
+
+class NotesFetchingState extends NotesState {}
+
+class NotesFetchedState extends NotesState {
+  final List<NoteModel> notes;
+  NotesFetchedState(this.notes);
+}
+
+class NotesFetchingFailedState extends NotesState {
+  final String reason;
+
+  NotesFetchingFailedState(this.reason);
+}
+
+class NotesLoadingState extends NotesState{}
+
+class NotesLoadedState extends NotesState{}
+
+class NotesEmptyState extends NotesState {}
+
+
