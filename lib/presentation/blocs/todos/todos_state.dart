eae153f5960844e27fb4849062f4d1affbd36c23
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-08-03 18:33:55 +0530

    Update todos_state.dart

--- lib/presentation/blocs/todos/todos_state.dart
+++ lib/presentation/blocs/todos/todos_state.dart
@@ -3,4 +3,27 @@ part of 'todos_bloc.dart';
 @immutable
 abstract class TodosState {}
 
-class TodosInitial extends TodosState {}
+abstract class TodosActionState extends TodosState {}
+
+class TodosInitialState extends TodosState {}
+
+class TodosFetchingState extends TodosState {}
+
+class TodosFetchedState extends TodosState {
+  final List<TodoModel> todos;
+  TodosFetchedState(this.todos);
+}
+
+class TodosFetchingFailedState extends TodosState {
+  final String reason;
+
+  TodosFetchingFailedState(this.reason);
+}
+
+class TodosLoadingState extends TodosState{}
+
+class TodosLoadedState extends TodosState{}
+
+class TodosEmptyState extends TodosState {}
+
+
