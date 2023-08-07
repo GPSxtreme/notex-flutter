1cdbe19ea8c52d37cabf9cb34476457d311e7054
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-08-07 22:36:36 +0530

    Update todos_state.dart

--- lib/presentation/blocs/todos/todos_state.dart
+++ lib/presentation/blocs/todos/todos_state.dart
@@ -12,8 +12,15 @@ class TodosFetchingState extends TodosState {}
 class TodosFetchedState extends TodosState {
   final List<TodoModel> doneTodos;
   final List<TodoModel> notDoneTodos;
+  final bool isInEditState;
+  final bool areAllSelected;
 
-  TodosFetchedState(this.doneTodos, this.notDoneTodos);
+  TodosFetchedState(
+    this.doneTodos,
+    this.notDoneTodos, {
+    this.isInEditState = false,
+    this.areAllSelected = false,
+  });
 }
 
 class TodosFetchingFailedState extends TodosState {
@@ -36,7 +43,25 @@ class TodosShowAddTodoDialogBoxState extends TodosActionState {}
 
 class TodosAddTodoSuccessState extends TodosActionState {}
 
-class TodosAddTodoFailedState extends TodosActionState{
+class TodosAddTodoFailedState extends TodosActionState {
   final String reason;
+
   TodosAddTodoFailedState(this.reason);
+}
+
+class TodosOperationFailedState extends TodosActionState {
+  final String reason;
+  TodosOperationFailedState(this.reason);
+}
+
+class TodosEnteredEditingState extends TodosActionState {}
+
+class TodosExitedEditingState extends TodosActionState {}
+
+class TodosManageAnimationsOfRemoved extends TodosActionState {
+  final List<TodoModel> removedTodos;
+  final List<TodoModel> doneTodos;
+  final List<TodoModel> notDoneTodos;
+
+  TodosManageAnimationsOfRemoved(this.removedTodos, this.doneTodos, this.notDoneTodos);
 }
