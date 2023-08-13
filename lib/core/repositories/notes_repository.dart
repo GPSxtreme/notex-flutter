6b4fd1931b02317415fe54356baa7e491dfc6a9f
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-08-13 19:40:55 +0530

    Implemented primitive server syncing
    - adding , updating or deleting a note now can be synced to server.
    - need to segregate the methods to make it more efficient

--- lib/core/repositories/notes_repository.dart
+++ lib/core/repositories/notes_repository.dart
@@ -1,8 +1,12 @@
+import 'dart:convert';
+
 import 'package:notex/core/repositories/shared_preferences_repository.dart';
+import 'package:notex/data/models/generic_server_response.dart';
 import 'package:notex/data/models/get_notes_response_model.dart';
 import 'package:http/http.dart' as http;
 import 'package:notex/data/repositories/entitiy_to_json_repository.dart';
 import 'package:notex/data/repositories/model_to_entity_repository.dart';
+import '../../data/models/add_note_response_model.dart';
 import '../../data/models/note_model.dart';
 import '../config/api_routes.dart';
 import '../../main.dart';
@@ -35,13 +39,13 @@ class NotesRepository {
     final Map<String, NoteModel> offlineNotesMap = {
       for (var note in offlineNotes) note.id: note
     };
-
     for (final onlineNote in onlineNotes) {
       if (!offlineNotesMap.containsKey(onlineNote.id)) {
         // Note doesn't exist offline, insert it
         offlineNotesMap[onlineNote.id] = NoteModel.fromJsonOfLocalDb(
             EntityToJson.noteEntityToJson(
-                ModelToEntityRepository.mapToNoteEntity(model: onlineNote,synced: true),
+                ModelToEntityRepository.mapToNoteEntity(
+                    model: onlineNote, synced: true),
                 true));
         await LOCAL_DB.insertNote(
           ModelToEntityRepository.mapToNoteEntity(model: onlineNote),
@@ -63,14 +67,37 @@ class NotesRepository {
 
     // Convert the Map values back to a List
     final updatedOfflineNotesList = offlineNotesMap.values.toList();
-
     return updatedOfflineNotesList;
   }
 
   static Future<void> addNote(NoteModel note) async {
     try {
+      // add note to local db
       await LOCAL_DB.insertNote(
           ModelToEntityRepository.mapToNoteEntity(model: note), false);
+      // add note on server
+      if (true) {
+        // if user enables auto sync.
+        final url = Uri.parse(NOTE_ADD_ROUTE);
+        final body = jsonEncode(note.toJsonToServerAdd());
+        final authToken = await SharedPreferencesRepository.getJwtToken();
+        final response = await http.post(
+          url,
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer $authToken'
+          },
+          body: body,
+        );
+        final GenericServerResponse serverResponse =
+            genericServerResponseFromJson(response.body);
+        if (serverResponse.success) {
+          final AddNoteResponseModel fetchResponse =
+              addNoteResponseModelFromJson(response.body);
+          // update local note id with the fetchResponse's
+          await LOCAL_DB.updateNoteId(note.id, fetchResponse.noteId);
+        }
+      }
     } catch (error) {
       rethrow;
     }
@@ -79,6 +106,19 @@ class NotesRepository {
   static Future<void> removeNote(String noteId) async {
     try {
       await LOCAL_DB.removeNote(noteId);
+      // remove note on server
+      if (true) {
+        // if user enables auto sync.
+        final url = Uri.parse("$NOTE_DELETE_ROUTE?noteId=$noteId");
+        final authToken = await SharedPreferencesRepository.getJwtToken();
+        await http.get(
+          url,
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer $authToken'
+          },
+        );
+      }
     } catch (error) {
       rethrow;
     }
@@ -87,6 +127,28 @@ class NotesRepository {
   static Future<void> updateNote(NoteModel note) async {
     try {
       LOCAL_DB.updateNote(ModelToEntityRepository.mapToNoteEntity(model: note));
+      // update note on server
+      if (true) {
+        // if user enables auto sync.
+        final url = Uri.parse(NOTE_UPDATE_ROUTE);
+        final body = jsonEncode(note.toJsonToServerUpdate());
+        final authToken = await SharedPreferencesRepository.getJwtToken();
+        final response = await http.post(
+          url,
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer $authToken'
+          },
+          body: body,
+        );
+        final GenericServerResponse fetchResponse =
+            genericServerResponseFromJson(response.body);
+        if (fetchResponse.success) {
+          await LOCAL_DB.setNoteSynced(note.id, true);
+        } else {
+          await LOCAL_DB.setNoteSynced(note.id, false);
+        }
+      }
     } catch (error) {
       rethrow;
     }
