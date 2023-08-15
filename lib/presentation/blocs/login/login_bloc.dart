3e120e409469a3e68b5a529b5f198081fcb55e56
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-08-15 11:42:23 +0530

    Update login_bloc.dart

--- lib/presentation/blocs/login/login_bloc.dart
+++ lib/presentation/blocs/login/login_bloc.dart
@@ -2,7 +2,9 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:notex/core/repositories/auth_repository.dart';
+
 part 'login_event.dart';
+
 part 'login_state.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
@@ -10,25 +12,30 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
     on<LoginPageLoginButtonClickedEvent>(processLogin);
     on<LoginPageRegisterButtonClickedEvent>(processRedirectToRegisterPage);
   }
-  FutureOr<void> processLogin(LoginPageLoginButtonClickedEvent event, Emitter<LoginState> emit) async{
+
+  FutureOr<void> processLogin(
+      LoginPageLoginButtonClickedEvent event, Emitter<LoginState> emit) async {
     // login user
-    try{
+    try {
       emit(LoginLoadingSate());
-      await AuthRepository.loginUser(event.email, event.password, event.rememberDevice).then(
-          (response) {
-            if(response.success){
-              emit(LoginSuccessState());
-            } else{
-              emit(LoginFailedState(response.message));
-            }
-          }
-      );
+      await AuthRepository.loginUser(
+              event.email, event.password, event.rememberDevice)
+          .then((response) async {
+        if (response.success) {
+          await AuthRepository.initUserToken()
+              .then((_) => emit(LoginSuccessState()));
+        } else {
+          emit(LoginFailedState(response.message));
+        }
+      });
       emit(LoginLoadedState());
-    } catch(error){
+    } catch (error) {
       emit(LoginFailedState("An unexpected error occurred"));
     }
   }
-  FutureOr<void> processRedirectToRegisterPage (LoginPageRegisterButtonClickedEvent event, Emitter<LoginState> emit){
+
+  FutureOr<void> processRedirectToRegisterPage(
+      LoginPageRegisterButtonClickedEvent event, Emitter<LoginState> emit) {
     emit(LoginNavigateToRegisterPageActionState());
   }
 }
