dbd827c8e2084825118a9c2e1096b2ae9411b41d
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-08-16 22:00:12 +0530

    Update settings.dart

--- lib/presentation/pages/settings.dart
+++ lib/presentation/pages/settings.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:go_router/go_router.dart';
-import 'package:notex/core/repositories/shared_preferences_repository.dart';
+import 'package:notex/presentation/blocs/settings/settings_bloc.dart';
 import 'package:notex/presentation/styles/app_styles.dart';
+import 'package:notex/router/app_route_constants.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
@@ -12,79 +14,140 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
-  late bool _autoSync;
-  bool _isLoading = true;
+  final SettingsBloc settingsBloc = SettingsBloc();
+
   @override
   void initState() {
     super.initState();
-    getSettings();
+    settingsBloc.add(SettingsInitialEvent());
   }
 
-  getSettings()async{
-    setState(() {
-      _isLoading = true;
-    });
-    _autoSync = await SharedPreferencesRepository.getAutoSyncStatus() ?? false;
-    setState(() {
-      _isLoading = false;
-    });
+  @override
+  void dispose() {
+    settingsBloc.close();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        leading: Builder(
-          builder: (BuildContext context) {
-            return IconButton(
-              splashRadius: 20,
-              icon: const Icon(Icons.arrow_back),
-              onPressed: () { GoRouter.of(context).pop(); },
-              tooltip: MaterialLocalizations.of(context).backButtonTooltip,
-            );
-          },
-        ),
-        title: Text("Settings",style: kInter,),
-        backgroundColor: kPageBgStart,
-      ),
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: kPageBgGradient
-        ),
-        child: !_isLoading ? ListTileTheme(
-          textColor: kWhite,
-          iconColor: kPinkD1,
-          contentPadding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            children: [
-              ListTile(
-                leading: const Icon(Icons.sync,color: kPinkD1,size: 30,),
-                title: Text('Enable auto-sync',style: kInter.copyWith(fontSize: 15),),
-                subtitle: Text('This enables auto-sync for both notes and todos.',style: kInter.copyWith(color: kWhite75,fontSize: 12),),
-                trailing: Switch(
-                  activeColor: kPink,
-                  activeTrackColor: kPinkD1,
-                  inactiveThumbColor: kPinkD1,
-                  inactiveTrackColor: kPinkD2,
-                  value: _autoSync,
-                  onChanged: (value)async{
-                    setState(() {
-                      _autoSync = value;
-                    });
-                    await SharedPreferencesRepository.setAutoSyncStatus(value);
+    return BlocConsumer(
+      bloc: settingsBloc,
+      listenWhen: (previous, current) => current is SettingsActionState,
+      buildWhen: (previous, current) => current is! SettingsActionState,
+      listener: (context, state) {
+        if (state is SettingsOperationFailedState) {
+          kSnackBar(context, state.reason);
+        } else if(state is SettingsUserLogoutState){
+          GoRouter.of(context).goNamed(AppRouteConstants.loginRouteName);
+        }
+      },
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            elevation: 0,
+            leading: Builder(
+              builder: (BuildContext context) {
+                return IconButton(
+                  splashRadius: 20,
+                  icon: const Icon(Icons.arrow_back),
+                  onPressed: () {
+                    GoRouter.of(context).pop();
                   },
-                ),
-              )
-            ],
+                  tooltip: MaterialLocalizations.of(context).backButtonTooltip,
+                );
+              },
+            ),
+            title: Text(
+              "Settings",
+              style: kInter,
+            ),
+            backgroundColor: kPageBgStart,
           ),
-        ) : const Center(
-          child: SpinKitRing(
-            lineWidth: 3.0,
-            color: kPinkD1,
+          body: Container(
+            decoration: const BoxDecoration(gradient: kPageBgGradient),
+            child: Material(
+              color: Colors.transparent,
+              child: Column(
+                mainAxisAlignment: state is! SettingsFetchedState ? MainAxisAlignment.center : MainAxisAlignment.start,
+                children: [
+                  if (state is SettingsFetchingState) ...[
+                    const Center(
+                      child: SpinKitRing(
+                        lineWidth: 3.0,
+                        color: kPinkD1,
+                      ),
+                    )
+                  ] else if (state is SettingsFetchingFailedState) ...[
+                    Center(
+                      child: Text(
+                        'Something went wrong \n ${state.reason}',
+                        style: kInter,
+                        textAlign: TextAlign.center,
+                      ),
+                    )
+                  ] else if (state is SettingsFetchedState) ...[
+                    ListTileTheme(
+                      textColor: kWhite,
+                      iconColor: kPinkD1,
+                      contentPadding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Column(
+                        children: [
+                          ListTile(
+                            leading: const Icon(
+                              Icons.sync,
+                              color: kPinkD1,
+                              size: 30,
+                            ),
+                            title: Text(
+                              'Enable auto-sync',
+                              style: kInter.copyWith(fontSize: 15),
+                            ),
+                            subtitle: Text(
+                              'This enables auto-sync for both notes and todos.',
+                              style:
+                                  kInter.copyWith(color: kWhite75, fontSize: 12),
+                            ),
+                            trailing: Switch(
+                              activeColor: kPink,
+                              activeTrackColor: kPinkD1,
+                              inactiveThumbColor: kPinkD1,
+                              inactiveTrackColor: kPinkD2,
+                              value: state.isAutoSyncEnabled,
+                              onChanged: (value) async {
+                                settingsBloc.add(SettingsSetAutoSyncEvent(value));
+                              },
+                            ),
+                          ),
+                          ListTile(
+                            splashColor: kPinkD1,
+                            leading: const Icon(
+                              Icons.logout_rounded,
+                              color: kPinkD1,
+                              size: 30,
+                            ),
+                            onTap: (){
+                              settingsBloc.add(SettingsUserLogoutEvent());
+                            },
+                            title: Text(
+                              'Logout',
+                              style: kInter.copyWith(fontSize: 15),
+                            ),
+                            subtitle: Text(
+                              'You will be redirected to login screen',
+                              style:
+                              kInter.copyWith(color: kWhite75, fontSize: 12),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ]
+                ],
+              ),
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
