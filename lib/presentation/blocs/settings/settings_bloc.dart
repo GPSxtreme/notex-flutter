71e38e84ce30a480e05b8d4106ddca5add682609
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-08-19 21:56:26 +0530

    added user verify & password reset

--- lib/presentation/blocs/settings/settings_bloc.dart
+++ lib/presentation/blocs/settings/settings_bloc.dart
@@ -6,6 +6,7 @@ import 'package:notex/core/repositories/auth_repository.dart';
 import 'package:notex/core/repositories/shared_preferences_repository.dart';
 
 part 'settings_event.dart';
+
 part 'settings_state.dart';
 
 class SettingsBloc extends Bloc<SettingsEvent, SettingsState> {
@@ -13,36 +14,82 @@ class SettingsBloc extends Bloc<SettingsEvent, SettingsState> {
     on<SettingsInitialEvent>(handleFetchSettings);
     on<SettingsSetAutoSyncEvent>(handleSetAutoSync);
     on<SettingsUserLogoutEvent>(handleUserLogout);
+    on<SettingsUserAccountVerifyEvent>(handleUserVerification);
+    on<SettingsUserPasswordResetEvent>(handleUserPasswordReset);
   }
 
-  FutureOr<void> handleFetchSettings(SettingsInitialEvent event, Emitter<SettingsState> emit)async{
-    try{
-      final autoSyncStatus = await SharedPreferencesRepository.getAutoSyncStatus();
+  FutureOr<void> handleFetchSettings(
+      SettingsInitialEvent event, Emitter<SettingsState> emit) async {
+    try {
+      final autoSyncStatus =
+          await SharedPreferencesRepository.getAutoSyncStatus();
       emit(SettingsFetchedState(autoSyncStatus ?? false));
-    }catch(error){
+    } catch (error) {
       emit(SettingsFetchingFailedState(error.toString()));
     }
   }
 
-  FutureOr<void> handleSetAutoSync(SettingsSetAutoSyncEvent event , Emitter<SettingsState> emit)async{
-    try{
+  FutureOr<void> handleSetAutoSync(
+      SettingsSetAutoSyncEvent event, Emitter<SettingsState> emit) async {
+    try {
       await SharedPreferencesRepository.setAutoSyncStatus(event.value);
       emit(SettingsFetchedState(event.value));
-    }catch(error){
-      emit(SettingsOperationFailedState(error.toString()));
+    } catch (error) {
+      emit(SettingsSnackBarState(error.toString()));
     }
   }
 
-  FutureOr<void> handleUserLogout(SettingsUserLogoutEvent event , Emitter<SettingsState> emit)async{
-    try{
+  FutureOr<void> handleUserLogout(
+      SettingsUserLogoutEvent event, Emitter<SettingsState> emit) async {
+    try {
       final response = await AuthRepository.logoutUser();
-      if(response){
-        emit(SettingsUserLogoutState());
-      }else{
-        emit(SettingsOperationFailedState("Failed to logout user"));
+      if (response) {
+        emit(SettingsUserLogoutState(body: event.body, title: event.title));
+      } else {
+        emit(SettingsSnackBarState("Failed to logout user"));
       }
-    } catch(error){
-      emit(SettingsOperationFailedState(error.toString()));
+    } catch (error) {
+      emit(SettingsSnackBarState(error.toString()));
+    }
+  }
+
+  FutureOr<void> handleUserVerification(
+      SettingsUserAccountVerifyEvent event, Emitter<SettingsState> emit) async {
+    try {
+      await AuthRepository.sendAccountVerificationEmail().then((res) async {
+        if (res.success) {
+          await AuthRepository.logoutUser();
+          emit(SettingsUserLogoutState(
+              title: 'Email sent!',
+              body: 'You will be logged out to reinitialise login credentials.\n(check spam folder for link if not in inbox)',
+              agreeLabel: 'continue',
+              isBarrierDismissible: false,
+              isSingleButton: true));
+        } else {
+          emit(SettingsSnackBarState(res.message));
+        }
+      });
+    } catch (error) {
+      emit(SettingsSnackBarState(error.toString()));
+    }
+  }
+  FutureOr<void> handleUserPasswordReset (SettingsUserPasswordResetEvent event, Emitter<SettingsState> emit )async{
+    try{
+      await AuthRepository.sendPasswordResetLink().then((res) async {
+        if (res.success) {
+          await AuthRepository.logoutUser();
+          emit(SettingsUserLogoutState(
+              title: 'Email sent!',
+              body: 'You will be logged out to reinitialise login credentials.\n(check spam folder for link if not in inbox)',
+              agreeLabel: 'continue',
+              isBarrierDismissible: false,
+              isSingleButton: true));
+        } else {
+          emit(SettingsSnackBarState(res.message));
+        }
+      });
+    }catch(error){
+      emit(SettingsSnackBarState(error.toString()));
     }
   }
 }
