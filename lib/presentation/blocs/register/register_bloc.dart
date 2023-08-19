2650698a9265d0ea67a346e0765685909c8518b0
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-08-19 21:56:10 +0530

    Update register_bloc.dart

--- lib/presentation/blocs/register/register_bloc.dart
+++ lib/presentation/blocs/register/register_bloc.dart
@@ -4,7 +4,10 @@ import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:notex/core/repositories/auth_repository.dart';
 
+import '../../../main.dart';
+
 part 'register_event.dart';
+
 part 'register_state.dart';
 
 class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
@@ -15,33 +18,41 @@ class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
     on<RegisterPagePasswordsDoNotMatchEvent>(processPasswordsDoNotMatchError);
   }
 
-  FutureOr<void> processRegisterUser(RegisterPageRegisterButtonPressedEvent event, Emitter<RegisterState> emit) async{
-    try{
+  FutureOr<void> processRegisterUser(
+      RegisterPageRegisterButtonPressedEvent event,
+      Emitter<RegisterState> emit) async {
+    try {
       emit(RegisterLoadingState());
-      await AuthRepository.registerUser(event.email, event.password, event.rememberDevice).then(
-              (response) async{
-            if(response.success){
-              await AuthRepository.initUserToken().then(
-                  (_) =>
-                      emit(RegisterSuccessState())
-              );
-            } else{
-              emit(RegisterFailedState(response.message));
-            }
-          }
-      );
+      await AuthRepository.registerUser(
+              event.email, event.password, event.rememberDevice)
+          .then((response) async {
+        if (response.success) {
+          await AuthRepository.initUserToken().then((_) async {
+            await USER.init();
+            emit(RegisterSuccessState());
+          });
+        } else {
+          emit(RegisterFailedState(response.message));
+        }
+      });
       emit(RegisterLoadedState());
-    } catch(error){
+    } catch (error) {
       emit(RegisterFailedState("An unexpected error occurred"));
     }
   }
-  FutureOr<void> processRedirectToLoginPage(RegisterPageLoginButtonPressedEvent event, Emitter<RegisterState> emit) {
-  emit(RegisterRedirectToLoginState());
+
+  FutureOr<void> processRedirectToLoginPage(
+      RegisterPageLoginButtonPressedEvent event, Emitter<RegisterState> emit) {
+    emit(RegisterRedirectToLoginState());
   }
-  FutureOr<void> processEmptyCredentialsError(RegisterPageEmptyCredentialsEvent event, Emitter<RegisterState> emit) {
+
+  FutureOr<void> processEmptyCredentialsError(
+      RegisterPageEmptyCredentialsEvent event, Emitter<RegisterState> emit) {
     emit(RegisterEmptyCredentialsState());
   }
-  FutureOr<void> processPasswordsDoNotMatchError(RegisterPagePasswordsDoNotMatchEvent event, Emitter<RegisterState> emit) {
+
+  FutureOr<void> processPasswordsDoNotMatchError(
+      RegisterPagePasswordsDoNotMatchEvent event, Emitter<RegisterState> emit) {
     emit(RegisterPasswordsDoNotMatchState());
   }
 }
