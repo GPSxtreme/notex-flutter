408c8e0d76bda081cce68af9351e091457122f63
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-08-22 19:18:00 +0530

    Update settings_bloc.dart

--- lib/presentation/blocs/settings/settings_bloc.dart
+++ lib/presentation/blocs/settings/settings_bloc.dart
@@ -1,9 +1,8 @@
 import 'dart:async';
-
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:notex/core/repositories/auth_repository.dart';
-import 'package:notex/core/repositories/shared_preferences_repository.dart';
+import '../../../main.dart';
 
 part 'settings_event.dart';
 
@@ -16,14 +15,17 @@ class SettingsBloc extends Bloc<SettingsEvent, SettingsState> {
     on<SettingsUserLogoutEvent>(handleUserLogout);
     on<SettingsUserAccountVerifyEvent>(handleUserVerification);
     on<SettingsUserPasswordResetEvent>(handleUserPasswordReset);
+    on<SettingsSetPrefetchCloudNotesEvent>(handleSetOnlineNotesPrefetch);
+    on<SettingsSetPrefetchCloudTodosEvent>(handleSetOnlineTodosPrefetch);
   }
 
   FutureOr<void> handleFetchSettings(
       SettingsInitialEvent event, Emitter<SettingsState> emit) async {
     try {
-      final autoSyncStatus =
-          await SharedPreferencesRepository.getAutoSyncStatus();
-      emit(SettingsFetchedState(autoSyncStatus ?? false));
+      emit(SettingsFetchedState(
+          isAutoSyncEnabled: SETTINGS.isAutoSyncEnabled,
+          isNotesOnlinePrefetchEnabled: SETTINGS.isNotesOnlinePrefetchEnabled,
+          isTodosOnlinePrefetchEnabled: SETTINGS.isTodosOnlinePrefetchEnabled));
     } catch (error) {
       emit(SettingsFetchingFailedState(error.toString()));
     }
@@ -32,8 +34,35 @@ class SettingsBloc extends Bloc<SettingsEvent, SettingsState> {
   FutureOr<void> handleSetAutoSync(
       SettingsSetAutoSyncEvent event, Emitter<SettingsState> emit) async {
     try {
-      await SharedPreferencesRepository.setAutoSyncStatus(event.value);
-      emit(SettingsFetchedState(event.value));
+      await SETTINGS.setAutoSyncEnabled(event.value);
+      emit(SettingsFetchedState(
+          isAutoSyncEnabled: SETTINGS.isAutoSyncEnabled,
+          isNotesOnlinePrefetchEnabled: SETTINGS.isNotesOnlinePrefetchEnabled,
+          isTodosOnlinePrefetchEnabled: SETTINGS.isTodosOnlinePrefetchEnabled));
+    } catch (error) {
+      emit(SettingsSnackBarState(error.toString()));
+    }
+  }
+  FutureOr<void> handleSetOnlineNotesPrefetch(
+      SettingsSetPrefetchCloudNotesEvent event, Emitter<SettingsState> emit) async {
+    try {
+      await SETTINGS.setNotesOnlinePrefetch(event.value);
+      emit(SettingsFetchedState(
+          isAutoSyncEnabled: SETTINGS.isAutoSyncEnabled,
+          isNotesOnlinePrefetchEnabled: SETTINGS.isNotesOnlinePrefetchEnabled,
+          isTodosOnlinePrefetchEnabled: SETTINGS.isTodosOnlinePrefetchEnabled));
+    } catch (error) {
+      emit(SettingsSnackBarState(error.toString()));
+    }
+  }
+  FutureOr<void> handleSetOnlineTodosPrefetch(
+      SettingsSetPrefetchCloudTodosEvent event, Emitter<SettingsState> emit) async {
+    try {
+      await SETTINGS.setTodosOnlinePrefetch(event.value);
+      emit(SettingsFetchedState(
+          isAutoSyncEnabled: SETTINGS.isAutoSyncEnabled,
+          isNotesOnlinePrefetchEnabled: SETTINGS.isNotesOnlinePrefetchEnabled,
+          isTodosOnlinePrefetchEnabled: SETTINGS.isTodosOnlinePrefetchEnabled));
     } catch (error) {
       emit(SettingsSnackBarState(error.toString()));
     }
@@ -61,7 +90,8 @@ class SettingsBloc extends Bloc<SettingsEvent, SettingsState> {
           await AuthRepository.logoutUser();
           emit(SettingsUserLogoutState(
               title: 'Email sent!',
-              body: 'You will be logged out to reinitialise login credentials.\n(check spam folder for link if not in inbox)',
+              body:
+                  'You will be logged out to reinitialise login credentials.\n(check spam folder for link if not in inbox)',
               agreeLabel: 'continue',
               isBarrierDismissible: false,
               isSingleButton: true));
@@ -73,14 +103,17 @@ class SettingsBloc extends Bloc<SettingsEvent, SettingsState> {
       emit(SettingsSnackBarState(error.toString()));
     }
   }
-  FutureOr<void> handleUserPasswordReset (SettingsUserPasswordResetEvent event, Emitter<SettingsState> emit )async{
-    try{
+
+  FutureOr<void> handleUserPasswordReset(
+      SettingsUserPasswordResetEvent event, Emitter<SettingsState> emit) async {
+    try {
       await AuthRepository.sendPasswordResetLink().then((res) async {
         if (res.success) {
           await AuthRepository.logoutUser();
           emit(SettingsUserLogoutState(
               title: 'Email sent!',
-              body: 'You will be logged out to reinitialise login credentials.\n(check spam folder for link if not in inbox)',
+              body:
+                  'You will be logged out to reinitialise login credentials.\n(check spam folder for link if not in inbox)',
               agreeLabel: 'continue',
               isBarrierDismissible: false,
               isSingleButton: true));
@@ -88,7 +121,7 @@ class SettingsBloc extends Bloc<SettingsEvent, SettingsState> {
           emit(SettingsSnackBarState(res.message));
         }
       });
-    }catch(error){
+    } catch (error) {
       emit(SettingsSnackBarState(error.toString()));
     }
   }
