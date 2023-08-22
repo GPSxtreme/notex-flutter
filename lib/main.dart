b50399eb20d9c12e6092433832970ca87d4f9453
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-08-22 19:17:56 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,15 @@ import 'package:notex/core/repositories/local_database_repository.dart';
 import 'package:notex/presentation/styles/app_styles.dart';
 import 'app.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-
+import 'data/datasources/settngs_data.dart';
 import 'data/datasources/user_data.dart';
 
 LocalDatabaseRepository LOCAL_DB = LocalDatabaseRepository();
 User USER = User();
+Settings SETTINGS = Settings();
 
 void main() async{
   await dotenv.load();
-  // print({"time":DateTime.now().toIso8601String()});
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: kPageBgStart,
   ));
