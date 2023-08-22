ae903cc901015cea97dc85e502eea0d13dd911d0
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-08-22 19:18:04 +0530

    Update settings_state.dart

--- lib/presentation/blocs/settings/settings_state.dart
+++ lib/presentation/blocs/settings/settings_state.dart
@@ -3,34 +3,48 @@ part of 'settings_bloc.dart';
 @immutable
 abstract class SettingsState {}
 
-abstract class SettingsActionState extends SettingsState{}
+abstract class SettingsActionState extends SettingsState {}
 
 class SettingsInitial extends SettingsState {}
 
-class SettingsFetchingState extends SettingsState{}
+class SettingsFetchingState extends SettingsState {}
 
-class SettingsFetchingFailedState extends SettingsState{
+class SettingsFetchingFailedState extends SettingsState {
   final String reason;
+
   SettingsFetchingFailedState(this.reason);
 }
 
-class SettingsFetchedState extends SettingsState{
+class SettingsFetchedState extends SettingsState {
   final bool isAutoSyncEnabled;
+  final bool isNotesOnlinePrefetchEnabled;
+  final bool isTodosOnlinePrefetchEnabled;
 
-  SettingsFetchedState(this.isAutoSyncEnabled);
+  SettingsFetchedState(
+      {required this.isAutoSyncEnabled,
+      required this.isNotesOnlinePrefetchEnabled,
+      required this.isTodosOnlinePrefetchEnabled});
 }
 
-class SettingsSnackBarState extends SettingsActionState{
+class SettingsSnackBarState extends SettingsActionState {
   final String reason;
+
   SettingsSnackBarState(this.reason);
 }
 
-class SettingsUserLogoutState extends SettingsActionState{
+class SettingsUserLogoutState extends SettingsActionState {
   final String? title;
   final String? body;
   final String? agreeLabel;
   final String? disagreeLabel;
   final bool isSingleButton;
   final bool isBarrierDismissible;
-  SettingsUserLogoutState({this.title, this.body,this.agreeLabel, this.disagreeLabel, this.isSingleButton = false,this.isBarrierDismissible = true});
+
+  SettingsUserLogoutState(
+      {this.title,
+      this.body,
+      this.agreeLabel,
+      this.disagreeLabel,
+      this.isSingleButton = false,
+      this.isBarrierDismissible = true});
 }
