a2f83b3d56b6bd70316e73a415b9678f5113ab3f
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-08-27 23:25:53 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,27 @@
 // ignore_for_file: non_constant_identifier_names
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:notex/core/repositories/local_database_repository.dart';
 import 'package:notex/presentation/styles/app_styles.dart';
+import 'package:notex/services/notification.dart';
 import 'app.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'data/datasources/settngs_data.dart';
 import 'data/datasources/user_data.dart';
+import 'package:timezone/data/latest.dart' as tz;
 
 LocalDatabaseRepository LOCAL_DB = LocalDatabaseRepository();
 User USER = User();
 Settings SETTINGS = Settings();
+NotificationService NOTIFICATION_SERVICES = NotificationService();
 
 void main() async{
   await dotenv.load();
+  await NOTIFICATION_SERVICES.init();
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: kPageBgStart,
   ));
+  tz.initializeTimeZones();
   runApp(const MyApp());
 }
 
