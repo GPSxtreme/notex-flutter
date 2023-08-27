21a101a6cd760fe576c65312f73f8937b2379608
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-08-27 23:24:30 +0530

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      theme: ThemeData.dark(useMaterial3: true),
       debugShowCheckedModeBanner: false,
       routerConfig: MyAppRouter.getRouter(false),
     );
