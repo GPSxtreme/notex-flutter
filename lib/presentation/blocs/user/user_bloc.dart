34722d6e419132953e2fbc0e2bf3ece81897f5ec
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-08-29 02:23:45 +0530

    Create user_bloc.dart

---
+++ lib/presentation/blocs/user/user_bloc.dart
@@ -0,0 +1,82 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:bloc/bloc.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:meta/meta.dart';
+import 'package:notex/core/config/api_routes.dart';
+import 'package:notex/core/repositories/shared_preferences_repository.dart';
+import 'package:notex/core/repositories/user_repository.dart';
+import 'package:notex/data/models/updatable_user_data_model.dart';
+import 'package:notex/data/models/update_user_data_reponse_model.dart';
+import 'package:notex/data/models/user_model.dart';
+import '../../../core/repositories/auth_repository.dart';
+import '../../../main.dart';
+
+part 'user_event.dart';
+
+part 'user_state.dart';
+
+class UserBloc extends Bloc<UserEvent, UserState> {
+  UserBloc() : super(UserInitial()) {
+    on<UserInitialEvent>(handleFetchData);
+    on<UserUpdateUserDataEvent>(handleUpdateData);
+  }
+
+  final CachedNetworkImageProvider img = CachedNetworkImageProvider(
+    USER_PROFILE_PICTURE_GET_ROUTE,
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': AuthRepository.userToken
+    },
+    cacheKey: USER.profilePictureCacheKey,
+  );
+
+  FutureOr<void> handleFetchData(
+      UserInitialEvent event, Emitter<UserState> emit) async {
+    try {
+      emit(UserFetchingState());
+
+      emit(UserSettingsFetchedState(USER.data!, img));
+    } catch (error) {
+      emit(UserOperationFailedState(error.toString()));
+    }
+  }
+
+  FutureOr<void> handleUpdateData(
+      UserUpdateUserDataEvent event, Emitter<UserState> emit) async {
+    try {
+      emit(UserSettingsFetchedState(USER.data!, img,isUpdating: true));
+      // update profile picture
+      if(event.img != null){
+        bool response = await UserRepository.updateUserProfilePicture(event.img!);
+        if(!response){
+          emit(UserOperationFailedState("Failed updating profile picture"));
+        }else{
+          await CachedNetworkImage.evictFromCache(USER.profilePictureCacheKey);
+          await SharedPreferencesRepository.generateProfilePictureCacheKey();
+        }
+      }
+      if(event.data != null){
+        UpdateUserDataResponseModel response = await UserRepository.updateUserData(event.data!);
+        if(response.success){
+          emit(UserSendScaffoldMessageState(response.message));
+          // update login token
+          await SharedPreferencesRepository.saveJwtToken(response.token!);
+          await AuthRepository.initUserToken();
+          await USER.init().then(
+              (_) {
+                emit(UserSettingsFetchedState(USER.data!, img));
+              }
+          );
+        } else{
+          emit(UserUpdateUserDataFailedState(response.message));
+          emit(UserSettingsFetchedState(USER.data!, img));
+        }
+      }
+    } catch (error) {
+      emit(UserOperationFailedState(error.toString()));
+    } finally{
+      emit(UserResetAfterUpdateState());
+    }
+  }
+}
