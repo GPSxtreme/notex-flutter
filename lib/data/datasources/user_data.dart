fb47b24f387532c443b56e87b3a83c49ed772d1c
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-08-29 02:23:38 +0530

    Update user_data.dart

--- lib/data/datasources/user_data.dart
+++ lib/data/datasources/user_data.dart
@@ -1,8 +1,10 @@
 import 'package:notex/core/repositories/auth_repository.dart';
+import 'package:notex/core/repositories/shared_preferences_repository.dart';
 import 'package:notex/data/models/user_model.dart';
 
 class User{
   late UserDataModel? data;
+  late String profilePictureCacheKey;
   void setData(UserDataModel newData) => data = newData;
 
   Future<void> init()async{
@@ -11,6 +13,15 @@ class User{
           if(res != null){
            setData(res);
            print(data!.toJson());
+          }
+        }
+    );
+    await SharedPreferencesRepository.getProfilePictureCacheKey().then(
+        (key)async{
+          if(key == null){
+             profilePictureCacheKey = await SharedPreferencesRepository.generateProfilePictureCacheKey();
+          } else{
+            profilePictureCacheKey = key;
           }
         }
     );
