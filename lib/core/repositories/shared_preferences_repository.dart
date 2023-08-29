ffb2c0a7fc3b3b7bb1b7bb0eda337010892c31e9
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-08-29 21:03:15 +0530

    Update shared_preferences_repository.dart

--- lib/core/repositories/shared_preferences_repository.dart
+++ lib/core/repositories/shared_preferences_repository.dart
@@ -7,6 +7,10 @@ class SharedPreferencesRepository {
   static const String _enableAutoSync = 'enableAutoSync';
   static const String _enableNotesOnlinePrefetch = 'enableNotesOnlinePrefetch';
   static const String _enableTodosOnlinePrefetch = 'enableTodosOnlinePrefetch';
+  static const String _enableAppLock = 'enableAppLock';
+  static const String _enableHiddenNotesLock = 'enableHiddenNotesLock';
+  static const String _enableDeletedNotesLock = 'enableDeletedNotesLock';
+  static const String _enableBiometricOnly = 'enableBiometricOnly';
 
   /// Method to save the JWT token to shared preferences
   static Future<void> saveJwtToken(String token) async {
@@ -74,5 +78,61 @@ class SharedPreferencesRepository {
   static Future<String?> getProfilePictureCacheKey()async{
     final preferences = await SharedPreferences.getInstance();
     return preferences.getString(_profilePictureCacheKey);
+  }
+
+  /// Method to set app lock status.
+  /// [value] is the boolean value to set for app lock.
+  static Future<void> setAppLockStatus(bool value) async {
+    final preferences = await SharedPreferences.getInstance();
+    await preferences.setBool(_enableAppLock, value);
+  }
+
+  /// Method to get if app lock is enabled.
+  /// Returns `true` if app lock is enabled, otherwise `false`.
+  static Future<bool?> getAppLockStatus() async {
+    final preferences = await SharedPreferences.getInstance();
+    return preferences.getBool(_enableAppLock);
+  }
+
+  /// Method to set hidden notes lock status.
+  /// [value] is the boolean value to set for hidden notes lock.
+  static Future<void> setHiddenNotesLockStatus(bool value) async {
+    final preferences = await SharedPreferences.getInstance();
+    await preferences.setBool(_enableHiddenNotesLock, value);
+  }
+
+  /// Method to get if hidden notes lock is enabled.
+  /// Returns `true` if hidden notes lock is enabled, otherwise `false`.
+  static Future<bool?> getHiddenNotesLockStatus() async {
+    final preferences = await SharedPreferences.getInstance();
+    return preferences.getBool(_enableHiddenNotesLock);
+  }
+
+  /// Method to set deleted notes lock status.
+  /// [value] is the boolean value to set for deleted notes lock.
+  static Future<void> setDeletedNotesLockStatus(bool value) async {
+    final preferences = await SharedPreferences.getInstance();
+    await preferences.setBool(_enableDeletedNotesLock, value);
+  }
+
+  /// Method to get if deleted notes lock is enabled.
+  /// Returns `true` if deleted notes lock is enabled, otherwise `false`.
+  static Future<bool?> getDeletedNotesLockStatus() async {
+    final preferences = await SharedPreferences.getInstance();
+    return preferences.getBool(_enableDeletedNotesLock);
+  }
+
+  /// Method to set biometric only status.
+  /// [value] is the boolean value to set for biometric only.
+  static Future<void> setBiometricOnlyStatus(bool value) async {
+    final preferences = await SharedPreferences.getInstance();
+    await preferences.setBool(_enableBiometricOnly, value);
+  }
+
+  /// Method to get if biometric only is enabled.
+  /// Returns `true` if biometric only is enabled, otherwise `false`.
+  static Future<bool?> getBiometricOnlyStatus() async {
+    final preferences = await SharedPreferences.getInstance();
+    return preferences.getBool(_enableBiometricOnly);
   }
 }
