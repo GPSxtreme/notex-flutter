9f922952fc147b207d90081347d14943c8f8929f
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-08-29 21:03:09 +0530

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/gpsxtreme/notex/notex/MainActivity.kt
+++ android/app/src/main/kotlin/com/gpsxtreme/notex/notex/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.gpsxtreme.notex.notex
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
