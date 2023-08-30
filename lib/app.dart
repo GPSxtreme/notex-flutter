40a6be4dba018e830a675126ad73eff3099e8350
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-08-30 21:11:44 +0530

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -9,8 +9,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       theme: ThemeData.dark(useMaterial3: true),
+      darkTheme: ThemeData(
+          useMaterial3: true,
+          appBarTheme:
+              const AppBarTheme(iconTheme: IconThemeData(color: kWhite)),
+          bottomNavigationBarTheme: BottomNavigationBarThemeData(
+              showUnselectedLabels: false,
+              selectedItemColor: kPink,
+              backgroundColor: kPinkD1,
+              elevation: 0,
+              selectedLabelStyle: kInter.copyWith(color: kWhite)),
+          listTileTheme: const ListTileThemeData(horizontalTitleGap: 30)),
       debugShowCheckedModeBanner: false,
-      routerConfig: MyAppRouter.getRouter(false),
+      routerConfig: MyAppRouter.getRouter(),
     );
   }
 }
