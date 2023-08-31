453f5db8c5e1c614831b3b01402d8163ea4e9121
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-08-31 21:32:41 +0530

    Update create_user_profile_bloc.dart

--- lib/presentation/blocs/create_user_profile/create_user_profile_bloc.dart
+++ lib/presentation/blocs/create_user_profile/create_user_profile_bloc.dart
@@ -3,39 +3,42 @@ import 'dart:io';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:notex/core/repositories/user_repository.dart';
-
+import '../../../main.dart';
 import '../../../data/models/updatable_user_data_model.dart';
 
 part 'create_user_profile_event.dart';
+
 part 'create_user_profile_state.dart';
 
-class CreateUserProfileBloc extends Bloc<CreateUserProfileEvent, CreateUserProfileState> {
+class CreateUserProfileBloc
+    extends Bloc<CreateUserProfileEvent, CreateUserProfileState> {
   CreateUserProfileBloc() : super(CreateUserProfileInitial()) {
-    on<CreateUserProfileOpenDatePickerEvent>((event, emit) => emit(CreateUserProfileOpenDatePickerState()));
+    on<CreateUserProfileOpenDatePickerEvent>(
+        (event, emit) => emit(CreateUserProfileOpenDatePickerState()));
     on<CreateUserProfileCreateEvent>(handleUserProfileCreate);
   }
 
-  FutureOr<void> handleUserProfileCreate(CreateUserProfileCreateEvent event,emit)async{
+  FutureOr<void> handleUserProfileCreate(
+      CreateUserProfileCreateEvent event, emit) async {
     // upload profile picture
-    try{
+    try {
       emit(CreateUserProfileLoadingState());
-      await UserRepository.updateUserProfilePicture(event.imageFile).then(
-          (_)async{
-            // update remaining data
-            await UserRepository.updateUserData(event.data).then(
-                (response) {
-                  if(response.success){
-                    emit(CreateUserProfileSuccessState());
-                  }else{
-                    emit(CreateUserProfileFailedState(response.message));
-                  }
-                }
-            );
+      await UserRepository.updateUserProfilePicture(event.imageFile)
+          .then((_) async {
+        // update remaining data
+        await UserRepository.updateUserData(event.data).then((response) async {
+          if (response.success) {
+            await USER
+                .init()
+                .then((_) => emit(CreateUserProfileSuccessState()));
+          } else {
+            emit(CreateUserProfileFailedState(response.message));
           }
-      );
-    }catch(e){
+        });
+      });
+    } catch (e) {
       emit(CreateUserProfileFailedState('Unexpected error occurred : $e'));
-    } finally{
+    } finally {
       emit(CreateUserProfileLoadedState());
     }
   }
