90467dc1cd30f5062b597008f21efc8f7ae83948
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-12-14 22:17:44 +0530

    Update api_routes.dart

--- lib/core/config/api_routes.dart
+++ lib/core/config/api_routes.dart
@@ -2,29 +2,29 @@
 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-final _apiEndPoint = dotenv.env["API_ENDPOINT"];
+final apiEndPoint = dotenv.env["API_ENDPOINT"];
 /* start of user routes */
-final USER_LOGIN_ROUTE = '$_apiEndPoint/user/login';
-final USER_REGISTER_ROUTE = '$_apiEndPoint/user/registration';
-final USER_PROFILE_PICTURE_UPLOAD_ROUTE = '$_apiEndPoint/data/uploadProfilePicture';
-final USER_PROFILE_PICTURE_GET_ROUTE = '$_apiEndPoint/data/getProfilePicture';
-final USER_UPDATE_USER_DATA = '$_apiEndPoint/user/updateUserData';
-final USER_ACCOUNT_VERIFY_ROUTE = '$_apiEndPoint/user/sendVerificationLink';
-final USER_PASSWORD_RESET_ROUTE = '$_apiEndPoint/user/sendPasswordResetLink';
+final USER_LOGIN_ROUTE = '$apiEndPoint/user/login';
+final USER_REGISTER_ROUTE = '$apiEndPoint/user/registration';
+final USER_PROFILE_PICTURE_UPLOAD_ROUTE = '$apiEndPoint/data/uploadProfilePicture';
+final USER_PROFILE_PICTURE_GET_ROUTE = '$apiEndPoint/data/getProfilePicture';
+final USER_UPDATE_USER_DATA = '$apiEndPoint/user/updateUserData';
+final USER_ACCOUNT_VERIFY_ROUTE = '$apiEndPoint/user/sendVerificationLink';
+final USER_PASSWORD_RESET_ROUTE = '$apiEndPoint/user/sendPasswordResetLink';
 /* end of user routes */
 
 /* start of todo_routes */
-final TODO_ADD_ROUTE = '$_apiEndPoint/todo/addTodo';
-final TODO_GET_ROUTE = '$_apiEndPoint/todo/getTodos';
+final TODO_ADD_ROUTE = '$apiEndPoint/todo/addTodo';
+final TODO_GET_ROUTE = '$apiEndPoint/todo/getTodos';
 /// Required todoId in params
-final TODO_DELETE_ROUTE = '$_apiEndPoint/todo/deleteTodo';
-final TODO_UPDATE_ROUTE = '$_apiEndPoint/todo/updateTodo';
+final TODO_DELETE_ROUTE = '$apiEndPoint/todo/deleteTodo';
+final TODO_UPDATE_ROUTE = '$apiEndPoint/todo/updateTodo';
 /* end of todo_routes */
 
 /*start of note routes */
-final NOTE_ADD_ROUTE = '$_apiEndPoint/note/addNote';
-final NOTE_GET_ROUTE = '$_apiEndPoint/note/getNotes';
-final NOTE_UPDATE_ROUTE = '$_apiEndPoint/note/updateNote';
+final NOTE_ADD_ROUTE = '$apiEndPoint/note/addNote';
+final NOTE_GET_ROUTE = '$apiEndPoint/note/getNotes';
+final NOTE_UPDATE_ROUTE = '$apiEndPoint/note/updateNote';
 /// Required noteId in params
-final NOTE_DELETE_ROUTE = '$_apiEndPoint/note/deleteNote';
+final NOTE_DELETE_ROUTE = '$apiEndPoint/note/deleteNote';
 /*end of note routes */
