2008a7e21b93e50e70f960cfd0ea1444258781b8
Prudhvi Suraaj <86603322+GPSxtreme@users.noreply.github.com>
2024-07-16 01:29:53 +0530

    Add AppColors class with predefined color constants

---
+++ lib/presentation/styles/app_colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color background = Color.fromARGB(255, 0, 0, 0);
+  static const Color foreground = Color.fromARGB(255, 249, 249, 249);
+  static const Color card = Color.fromARGB(255, 7, 7, 7);
+  static const Color cardForeground = Color.fromARGB(255, 249, 249, 249);
+  static const Color popover = Color.fromARGB(255, 7, 7, 7);
+  static const Color popoverForeground = Color.fromARGB(255, 249, 249, 249);
+  static const Color primary = Color.fromARGB(255, 117, 119, 255);
+  static const Color primaryForeground = Color.fromARGB(255, 5, 5, 5);
+  static const Color secondary = Color.fromARGB(255, 15, 15, 15);
+  static const Color secondaryForeground = Color.fromARGB(255, 249, 249, 249);
+  static const Color muted = Color.fromARGB(255, 22, 22, 22);
+  static const Color mutedForeground = Color.fromARGB(255, 124, 124, 124);
+  static const Color accent = Color.fromARGB(255, 15, 15, 15);
+  static const Color accentForeground = Color.fromARGB(255, 249, 249, 249);
+  static const Color destructive = Color.fromARGB(255, 238, 67, 67);
+  static const Color destructiveForeground = Color.fromARGB(255, 249, 249, 249);
+  static const Color border = Color.fromARGB(255, 28, 28, 28);
+  static const Color input = Color.fromARGB(255, 40, 40, 40);
+  static const Color ring = Color.fromARGB(255, 0, 3, 204);
+}
