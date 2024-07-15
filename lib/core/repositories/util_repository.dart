d281be2459d6d82a9fe7588a1bb7663927a41a91
Prudhvi Suraaj <86603322+GPSxtreme@users.noreply.github.com>
2024-07-16 01:28:11 +0530

    Refactor UtilRepository class for toast and email launch

--- lib/core/repositories/util_repository.dart
+++ lib/core/repositories/util_repository.dart
@@ -1,11 +1,9 @@
-
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:in_app_update/in_app_update.dart';
-import 'package:notex/presentation/styles/app_styles.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
-class UtilRepository{
+class UtilRepository {
   static void toast(String msg) {
     Fluttertoast.cancel();
     Fluttertoast.showToast(
@@ -13,8 +11,6 @@ class UtilRepository{
         toastLength: Toast.LENGTH_SHORT,
         gravity: ToastGravity.BOTTOM,
         timeInSecForIosWeb: 1,
-        backgroundColor: kPinkD2,
-        textColor: kWhite,
         fontSize: 16.0);
   }
 
@@ -32,25 +28,27 @@ class UtilRepository{
     return false;
   }
 
-
   ///Launches the given url
   static Future<void> launchLink(String url) async {
     try {
       final Uri uri = Uri.parse(url);
-      await launchUrl(uri,mode: LaunchMode.externalApplication);
+      await launchUrl(uri, mode: LaunchMode.externalApplication);
     } catch (e) {
       UtilRepository.toast(e.toString());
     }
   }
 
-  static Future<void> launchEmail({required List<String> emailAddresses, String? subject, String? body}) async {
+  static Future<void> launchEmail(
+      {required List<String> emailAddresses,
+      String? subject,
+      String? body}) async {
     final String emails = emailAddresses.join(',');
     String emailSubject = Uri.encodeComponent(subject ?? "");
     String emailBody = Uri.encodeComponent(body ?? "");
     String emailUrl = "mailto:$emails?subject=$emailSubject&body=$emailBody";
     try {
       await launchUrlString(emailUrl);
-    } catch(e) {
+    } catch (e) {
       UtilRepository.toast("Error redirecting to gmail.");
     }
   }
