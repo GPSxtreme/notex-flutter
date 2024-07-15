71aab01b74ed569d14cdbe5ce49866fe1d7403a9
Prudhvi Suraaj <86603322+GPSxtreme@users.noreply.github.com>
2024-07-16 01:28:34 +0530

    Update login page UI and navigation logic

--- lib/presentation/pages/login.dart
+++ lib/presentation/pages/login.dart
@@ -4,6 +4,7 @@ import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
 import 'package:notex/presentation/blocs/login/login_bloc.dart';
+import 'package:notex/presentation/styles/app_colors.dart';
 import 'package:notex/router/app_route_constants.dart';
 
 import '../styles/app_styles.dart';
@@ -28,57 +29,49 @@ class _LoginPageState extends State<LoginPage> {
     super.initState();
   }
 
-  Color getColor(Set<MaterialState> states) {
-    const Set<MaterialState> interactiveStates = <MaterialState>{
-      MaterialState.pressed,
-      MaterialState.hovered,
-      MaterialState.focused,
-    };
-    if (states.any(interactiveStates.contains)) {
-      return kPinkD1;
-    }
-    return kPinkD1;
-  }
-
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return BlocConsumer(
       bloc: loginBloc,
-      listenWhen: (previous,current) => current is LoginActionState,
-      buildWhen: (previous,current) => current is! LoginActionState,
-      listener: (context,state){
-        if(state is LoginNavigateToRegisterPageActionState){
+      listenWhen: (previous, current) => current is LoginActionState,
+      buildWhen: (previous, current) => current is! LoginActionState,
+      listener: (context, state) {
+        if (state is LoginNavigateToRegisterPageActionState) {
           GoRouter.of(context).pushNamed(AppRouteConstants.registerRouteName);
-        } else if(state is LoginSuccessState){
-          GoRouter.of(context).pushReplacementNamed(AppRouteConstants.homeRouteName);
-        } else if(state is LoginFailedState){
+        } else if (state is LoginSuccessState) {
+          GoRouter.of(context)
+              .pushReplacementNamed(AppRouteConstants.homeRouteName);
+        } else if (state is LoginFailedState) {
           kSnackBar(context, state.reason);
-        } else if ( state is LoginRedirectToCreateUserProfilePageAction){
-          GoRouter.of(context).pushNamed(AppRouteConstants.createUserProfileName);
-        } else if(state is LoginRedirectToPasswordResetPageAction){
-          GoRouter.of(context).pushNamed(AppRouteConstants.passwordResetRouteName);
+        } else if (state is LoginRedirectToCreateUserProfilePageAction) {
+          GoRouter.of(context)
+              .pushNamed(AppRouteConstants.createUserProfileName);
+        } else if (state is LoginRedirectToPasswordResetPageAction) {
+          GoRouter.of(context)
+              .pushNamed(AppRouteConstants.passwordResetRouteName);
         }
       },
-      builder: (context,state){
+      builder: (context, state) {
         return Scaffold(
           resizeToAvoidBottomInset: true,
-          backgroundColor: kPageBgStart,
           body: SafeArea(
             child: Container(
               width: SizeConfig.screenWidth,
               height: SizeConfig.screenHeight,
-              decoration: const BoxDecoration(gradient: kPageBgGradient),
               child: Stack(
                 children: [
                   Positioned(
                     top: 0,
                     left: -SizeConfig.blockSizeHorizontal! * 0.5,
-                    child: SvgPicture.asset('assets/svg/login_page_bg_decor.svg'),
+                    child:
+                        SvgPicture.asset('assets/svg/login_page_bg_decor.svg'),
                   ),
                   Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 0),
-                    child: NotificationListener<OverscrollIndicatorNotification>(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 25, vertical: 0),
+                    child:
+                        NotificationListener<OverscrollIndicatorNotification>(
                       onNotification: (overScroll) {
                         overScroll.disallowIndicator();
                         return true;
@@ -87,20 +80,19 @@ class _LoginPageState extends State<LoginPage> {
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            SizedBox(height: SizeConfig.blockSizeVertical! * 20,),
+                            SizedBox(
+                              height: SizeConfig.blockSizeVertical! * 20,
+                            ),
                             Text(
                               "Sign in to",
-                              style: kAppFont.copyWith(fontSize: 35),
                             ),
                             Row(
                               children: [
                                 Text(
                                   "your ",
-                                  style: kAppFont.copyWith(fontSize: 35),
                                 ),
                                 Text(
                                   "account",
-                                  style: kAppFont.copyWith(fontSize: 35, color: kPink),
                                 ),
                               ],
                             ),
@@ -112,16 +104,15 @@ class _LoginPageState extends State<LoginPage> {
                               children: [
                                 Text(
                                   "Don't have an account? ",
-                                  style: kAppFont.copyWith(fontSize: 18),
                                 ),
                                 GestureDetector(
                                     onTap: () {
                                       // go to register page
-                                      loginBloc.add(LoginPageRegisterButtonClickedEvent());
+                                      loginBloc.add(
+                                          LoginPageRegisterButtonClickedEvent());
                                     },
                                     child: Text(
                                       "Register ",
-                                      style: kAppFont.copyWith(fontSize: 18, color: kPink),
                                     )),
                               ],
                             ),
@@ -129,34 +120,16 @@ class _LoginPageState extends State<LoginPage> {
                               height: SizeConfig.blockSizeVertical! * 7,
                             ),
                             TextField(
-                              style: kAppFont.copyWith(fontSize: 18),
                               controller: _emailController,
                               keyboardType: TextInputType.emailAddress,
-                              cursorColor: kWhite,
-                              decoration:
-                              kTextFieldDecorationT1.copyWith(labelText: "Email"),
                             ),
                             SizedBox(
                               height: SizeConfig.blockSizeVertical! * 3,
                             ),
                             TextField(
-                              style: kAppFont.copyWith(fontSize: 18),
                               keyboardType: TextInputType.text,
                               controller: _passwordController,
-                              cursorColor: kWhite,
                               obscureText: _hide,
-                              decoration: kTextFieldDecorationT1.copyWith(
-                                  labelText: "Password",
-                                  suffixIcon: IconButton(
-                                      onPressed: () {
-                                        setState(() {
-                                          _hide = !_hide;
-                                        });
-                                      },
-                                      icon: Icon(
-                                        _hide ? Icons.visibility : Icons.visibility_off,
-                                        color: kWhite,
-                                      ))),
                             ),
                             SizedBox(
                               height: SizeConfig.blockSizeVertical,
@@ -164,14 +137,12 @@ class _LoginPageState extends State<LoginPage> {
                             Row(
                               children: [
                                 Transform.scale(
-                                  scale:1.3,
+                                  scale: 1.3,
                                   child: Checkbox(
                                       value: _rememberDevice,
-                                      checkColor: kPink,
                                       shape: RoundedRectangleBorder(
-                                          borderRadius: BorderRadius.circular(5)),
-                                      fillColor:
-                                      MaterialStateProperty.resolveWith(getColor),
+                                          borderRadius:
+                                              BorderRadius.circular(5)),
                                       onChanged: (bool? value) {
                                         setState(() {
                                           _rememberDevice = value!;
@@ -179,15 +150,13 @@ class _LoginPageState extends State<LoginPage> {
                                       }),
                                 ),
                                 GestureDetector(
-                                  onTap: (){
+                                  onTap: () {
                                     setState(() {
                                       _rememberDevice = !_rememberDevice;
                                     });
                                   },
                                   child: Text(
                                     "Remember device",
-                                    style: kAppFont.copyWith(
-                                        fontSize: 16, color: kWhite24),
                                   ),
                                 )
                               ],
@@ -198,20 +167,30 @@ class _LoginPageState extends State<LoginPage> {
                             SizedBox(
                               width: double.maxFinite,
                               child: ElevatedButton(
-                                onPressed: state is! LoginLoadingSate ? () {
-                                  // login user
-                                  if (_emailController.text.isEmpty ||
-                                      _passwordController.text.isEmpty) {
-                                    kSnackBar(context, 'Please fill in all fields');
-                                  } else {
-                                    loginBloc.add(LoginPageLoginButtonClickedEvent(_emailController.text, _passwordController.text,_rememberDevice));
-                                  }
-                                } : null,
-                                style: kBtnStyleT1,
-                                child: state is! LoginLoadingSate ? Text(
-                                  "Login",
-                                  style: kAppFont.copyWith(fontSize: 20),
-                                ) :const SpinKitCircle(color: kWhite,size: 22,),
+                                onPressed: state is! LoginLoadingSate
+                                    ? () {
+                                        // login user
+                                        if (_emailController.text.isEmpty ||
+                                            _passwordController.text.isEmpty) {
+                                          kSnackBar(context,
+                                              'Please fill in all fields');
+                                        } else {
+                                          loginBloc.add(
+                                              LoginPageLoginButtonClickedEvent(
+                                                  _emailController.text,
+                                                  _passwordController.text,
+                                                  _rememberDevice));
+                                        }
+                                      }
+                                    : null,
+                                child: state is! LoginLoadingSate
+                                    ? Text(
+                                        "Login",
+                                      )
+                                    : SpinKitCircle(
+                                        color: AppColors.primary,
+                                        size: 22,
+                                      ),
                               ),
                             ),
                             SizedBox(
@@ -219,19 +198,15 @@ class _LoginPageState extends State<LoginPage> {
                             ),
                             GestureDetector(
                               onTap: () {
-                                loginBloc.add(LoginPageForgotPasswordButtonClickedEvent());
+                                loginBloc.add(
+                                    LoginPageForgotPasswordButtonClickedEvent());
                               },
                               child: RichText(
-                                text: TextSpan(
-                                  text: 'Forgot',
-                                  style: kAppFont.copyWith(fontSize: 15),
-                                  children: [
-                                    TextSpan(
-                                      text: ' password?',
-                                      style: kAppFont.copyWith(fontSize: 15,color: kPink)
-                                    )
-                                  ]
-                                ),
+                                text: TextSpan(text: 'Forgot', children: [
+                                  TextSpan(
+                                    text: ' password?',
+                                  )
+                                ]),
                               ),
                             ),
                           ],
