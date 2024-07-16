fffc60e8d0aac258a75ecd31bb41ccc940023de8
Prudhvi Suraaj <86603322+GPSxtreme@users.noreply.github.com>
2024-07-16 20:13:49 +0530

    Update app.dart to use StatefulWidget and simplify router access

--- lib/app.dart
+++ lib/app.dart
@@ -3,16 +3,22 @@ import 'package:notex/presentation/styles/size_config.dart';
 import 'package:notex/presentation/styles/theme_data.dart';
 import 'package:notex/router/app_route_config.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final router = MyAppRouter.getRouter();
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return MaterialApp.router(
       darkTheme: defaultThemeData,
       debugShowCheckedModeBanner: false,
-      routerConfig: MyAppRouter.getRouter(),
+      routerConfig: router,
     );
   }
 }
