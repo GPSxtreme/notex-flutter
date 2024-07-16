0bb82b9a09e9344efea1991d86e9ca1175eadd7c
Prudhvi Suraaj <86603322+GPSxtreme@users.noreply.github.com>
2024-07-17 00:07:04 +0530

    Update login page to disable bottom inset resizing

--- lib/presentation/pages/login.dart
+++ lib/presentation/pages/login.dart
@@ -81,7 +81,7 @@ class _LoginPageState extends State<LoginPage> {
       },
       builder: (context, state) {
         return Scaffold(
-          resizeToAvoidBottomInset: true,
+          resizeToAvoidBottomInset: false,
           body: SafeArea(
             child: SizedBox(
               width: SizeConfig.screenWidth,
