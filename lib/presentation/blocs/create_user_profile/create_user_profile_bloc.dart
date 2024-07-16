e679f8ee934ab6246a0bbc92f1e5128a769acd1f
Prudhvi Suraaj <86603322+GPSxtreme@users.noreply.github.com>
2024-07-17 00:20:41 +0530

    Add validation for user's date of birth

--- lib/presentation/blocs/create_user_profile/create_user_profile_bloc.dart
+++ lib/presentation/blocs/create_user_profile/create_user_profile_bloc.dart
@@ -23,6 +23,15 @@ class CreateUserProfileBloc
     // upload profile picture
     try {
       emit(CreateUserProfileLoadingState());
+      if (event.data.dob.isAfter(DateTime.now())) {
+        emit(CreateUserProfileFailedState(
+            "Date of birth can't be in the future"));
+        return;
+      } else if (DateTime.now().difference(event.data.dob).inDays < 8 * 365) {
+        emit(CreateUserProfileFailedState(
+            "Date of birth must be at least 8 years old"));
+        return;
+      }
       await UserRepository.updateUserProfilePicture(event.imageFile)
           .then((_) async {
         // update remaining data
